da1c49a9a684b90c546442044a2a5d03ecb3bf19
MATYSIAK Hervé <fossdev@hervematysiak.eu>
2021-07-18 17:02:01 +0700

    feat(task): add inital `check` command

--- src/dotnetformat.ts
+++ src/dotnetformat.ts
@@ -1,17 +1,93 @@
+import os = require('os');
+import fs = require('fs');
+import path = require('path');
 import tl = require('azure-pipelines-task-lib/task');
+import tr = require("azure-pipelines-task-lib/toolrunner");
+import gitTool = require('./utils');
 
 async function run() {
     try {
-        const inputString: string | undefined = tl.getInput('samplestring', true);
-        if (inputString == 'bad') {
-            tl.setResult(tl.TaskResult.Failed, 'Bad input was given');
-            return;
+        const isDebug:boolean = Boolean(tl.getVariable("System.Debug"));
+        const isPullRequest = tl.getVariable("Build.Reason")! == "PullRequest";
+        const localWorkingPath = tl.getVariable("Build.Repository.LocalPath")!;
+        
+        // Get the tools 
+        const toolPath = tl.which("dotnet-format", true);
+        let toolRunOptions: tr.IExecOptions = {
+            cwd: localWorkingPath
+        };
+        let tool = tl.tool(toolPath);
+
+        if(isDebug){
+            await tl
+                .tool(toolPath)
+                .arg("--version")
+                .exec();
         }
-        console.log('Hello', inputString);
+
+        /// (TS) Tools setup
+        // TS:workspace
+        const workspaceOption = tl.getPathInput("workspaceOption", true)!;
+        const onlyChangedFiles = tl.getBoolInput("onlyChangedFiles");
+        if(workspaceOption == "" && onlyChangedFiles){
+            if(!isPullRequest){
+                console.error("`Build.Reason` != PullRequest, can't get the diff.")
+                tl.setResult(tl.TaskResult.Failed, "Can't find diff. between target and source branch, for PullRequest"); //TODO: better log
+                return;
+            }
+
+            const pullRequestTargetBranch = tl.getVariable("System.PullRequest.TargetBranch")!;
+            //TODO detect SCM
+            let gitScm = new gitTool.GitToolRunner();
+            const changeSet = gitScm.getChangeFor(pullRequestTargetBranch, '*.cs');
+            const rspFilePath = path.join(localWorkingPath, "FilesToCheck.rsp");
+            fs.writeFile(rspFilePath, changeSet.join(os.EOL), function (err) {
+                if (err) {
+                    return console.error(err);
+                }
+                console.debug(`FilesToCheck.rsp writen with ${changeSet.length} files`);
+            });
+            tool = tool.arg(['--include', '@FilesToCheck.rsp']);
+        } else {
+            if(fs.lstatSync(workspaceOption).isDirectory()){
+                tool = tool.arg(["-f", workspaceOption]);
+            } else {
+                tool = tool.arg(workspaceOption);
+            }            
+        }       
+
+        // TS:command
+        const commandToRun = tl.getInput("command", true);
+        switch (commandToRun) {
+            case "check":
+                tool = tool
+                    .arg("--check");
+                break;
+            case "custom":
+                tl.setResult(tl.TaskResult.Failed, "Custom command isn't impl. yet!");
+                return;
+            default:
+                tl.setResult(tl.TaskResult.Failed, "No command selected");
+                return;
+        }      
+        // TS:advanced options
+        const noRestoreOption = tl.getBoolInput("noRestoreOption", false);
+        if(noRestoreOption){
+            tool = tool
+                .arg("--no-restore");
+        }
+        
+        const verbosityOption = tl.getInput("verbosityOption", false);
+        if(verbosityOption){
+            tool = tool
+                .arg(['--verbosity', verbosityOption.toLowerCase()]);
+        }
+
+        var runReturnCode: number = await tool.exec(toolRunOptions);
+        console.log(runReturnCode);
     }
     catch (err) {
         tl.setResult(tl.TaskResult.Failed, err.message);
     }
 }
-
 run();
---
+++ src/utils/gittoolsrunner.ts
@@ -0,0 +1,34 @@
+import os = require('os');
+import tl = require("azure-pipelines-task-lib/task");
+import tr = require("azure-pipelines-task-lib/toolrunner");
+
+export class GitToolRunner {
+
+    public static readonly ToolName: string = "git"; 
+
+    private readonly repositoryDirectory: string;
+    private readonly gitToolPath: string;    
+
+    constructor(){
+        this.repositoryDirectory = tl.getPathInput(tl.getVariable("Build.Repository.LocalPath")!, false)!;
+        this.gitToolPath = tl.which(GitToolRunner.ToolName, true);
+    }
+
+    public getChangeFor(refBranch: Readonly<string>, filePattern: Readonly<string>): string[] {
+        let options: tr.IExecOptions = {
+            cwd: this.repositoryDirectory,
+            silent: true
+        };
+
+        try {
+            let result = tl.execSync(this.gitToolPath, `diff ${refBranch} --name-only -- ${filePattern}`, options);
+            console.debug(`Result: ${result.stdout}`)
+            tl.setResult(tl.TaskResult.Succeeded, `Git diff succeed ${result.code}`);
+            return result.stdout.split(os.EOL);
+        } catch (error) {
+            tl.error(error);
+            tl.setResult(tl.TaskResult.Failed, `Git diff failed ${error}`);
+        }
+        return [];
+    }
+}
