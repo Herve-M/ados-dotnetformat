e39caf1f95fd74c1748dbabb74d2f635e4fb985a
MATYSIAK Hervé <fossdev@hervematysiak.eu>
2023-10-11 19:22:53 +0700

    feat(usedotnetformat): initial empty task

---
+++ src/UseDotNetFormat/usedotnetformat.ts
@@ -0,0 +1,14 @@
+import path = require('path');
+import tl = require("azure-pipelines-task-lib/task");
+import toolLib = require("azure-pipelines-tool-lib/tool");
+
+async function run(){
+    try {
+
+    }
+    catch(err: any ){
+        tl.setResult(tl.TaskResult.Failed, err.message);
+    }
+}
+
+run(); 
