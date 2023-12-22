8bc581ec34eafbc739dbdadadad654c8090b619c
MATYSIAK Hervé <fossdev@hervematysiak.eu>
2023-12-22 14:46:44 +0700

    fix(cli): forgot to reaname name of input in conditional display

--- src/DotNetFormatCLI/test/succeed-check-partialPR-dbg-net6.ts
+++ src/DotNetFormatCLI/test/succeed-check-partialPR-dbg-net6.ts
@@ -6,16 +6,16 @@ import tmrm = require('azure-pipelines-task-lib/mock-run');
 const taskPath = path.join(__dirname, '..', 'dotnetformat.js');
 const  tmr: tmrm.TaskMockRunner = new tmrm.TaskMockRunner(taskPath);
 
-tmr.setInput('useGlobalToolOption', 'False');
+tmr.setInput('useGlobalTool', 'False');
 tmr.setInput('command', 'check');
-tmr.setInput('severityOption', 'error');
-tmr.setInput('workspaceOption', '');
+tmr.setInput('severity', 'error');
+tmr.setInput('workspace', '');
 tmr.setInput('onlyChangedFiles', 'True');
-tmr.setInput('noRestoreOption', 'False');
-tmr.setInput('includeGeneratedOption', 'False');
-tmr.setInput('diagnosticsOptions', '');
-tmr.setInput('diagnosticsExcludedOptions', '');
-tmr.setInput('verbosityOption', 'Quiet');
+tmr.setInput('noRestore', 'False');
+tmr.setInput('includeGenerated', 'False');
+tmr.setInput('diagnostics', '');
+tmr.setInput('diagnosticsExcluded', '');
+tmr.setInput('verbosity', 'Quiet');
 
 // .NET 6
 tmr.registerMock('azure-pipelines-task-lib/toolrunner', mtr);
@@ -29,6 +29,16 @@ const a: ma.TaskLibAnswers = <ma.TaskLibAnswers>{
         "c:\\path\\git.exe": true
     },
     "exec": {
+        "c:\\path\\git.exe remote show origin": {
+            "code": 0,
+            "stdout": "* remote origin",
+            "stderr": ""
+        },
+        "c:\\path\\git.exe branch": {
+            "code": 0,
+            "stdout": "* main",
+            "stderr": ""
+        },
         "c:\\path\\git.exe diff main --name-only -- *.cs": {
             "code": 0,
             "stdout": "src/DotNetFormat/dotnetformat.ts\nsrc/DotNetFormat/utils/gittoolsrunner.ts",
@@ -39,7 +49,7 @@ const a: ma.TaskLibAnswers = <ma.TaskLibAnswers>{
             "stdout": "6.4.352107+29f0a64560676efb3c24bc01942bcd84d807b335",
             "stderr": ""
         },
-        "c:\\path\\dotnet.exe format --include @FilesToCheck.rsp --severity error --verbosity quiet --binarylog c:/agent_work/1/a/Logs/format.binlog --verify-no-changes --report c:/agent_work/1/a/CodeAnalysisLogs/format.json": {
+        "c:\\path\\dotnet.exe format --include @c:/agent_work/1/s/FilesToCheck.rsp --severity error --verbosity quiet --binarylog c:/agent_work/1/a/Logs/format.binlog --verify-no-changes --report c:/agent_work/1/a/CodeAnalysisLogs/format.json": {
             "code": 0,
             "stdout": "OK",
             "stderr": ""
